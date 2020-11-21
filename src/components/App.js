28f737ea2e2b010a0f53e5bdbd689b2d1bf36ae3
Shreya <shreyaj2510@gmail.com>
2020-11-21 19:24:00 +0530

    ss

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,9 @@ import React, {Component, useState, useEffect} from "react";
 import '../styles/App.css';
 // import Slide from './Slide';
 
-const App = (props) => {
+const App = (prop) => {
   const [slide, setSlide] = useState(0);
-  const [disable, setDisable] = useState(false);
-
+  // const [disable, setDisable] = useState(false);
 
   const decr = () =>{
     setSlide(slide - 1);
@@ -23,12 +22,12 @@ const App = (props) => {
       <button id="Prev" data-testid="button-prev" onClick={decr}>Prev</button>
       :null}
 
-      {slide < props.slides.length-1 ? 
+      {slide < prop.slides.length-1 ? 
       <button id="Next" data-testid="button-next" onClick={incr}>Next</button>
       :null}
 
-      <h1 data-testid="title">{props.slides[slide].title}</h1>
-      <p data-testid="text">{props.slides[slide].text}</p>
+      <h1 data-testid="title">{prop.slides[slide].title}</h1>
+      <p data-testid="text">{prop.slides[slide].text}</p>
       {/* <Slide title={props.slides[slide].title} text={props.slides[slide].text}></Slide> */}
 
     </div>
