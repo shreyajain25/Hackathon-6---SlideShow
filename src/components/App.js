d879980caeb425b419f2117b3771e4dffc40aade
Shreya <shreyaj2510@gmail.com>
2020-11-21 17:39:23 +0530

    SLide Slow

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,29 @@
-import React, {Component, useState} from "react";
+import React, {Component, useState, useEffect} from "react";
 import '../styles/App.css';
 
-const App = () => {
+const App = (props) => {
+  const [slide, setSlide] = useState(0);
+  // useEffect(()=>{
+  //   return
+  // })
   return (
-    <></>
+    <div>
+      <h1 data-testid="title">{props.slides[slide].title}</h1>
+      <p data-testid="text">{props.slides[slide].text}</p>
+
+      {slide !== 0 ? 
+      <button data-testid="button-prev" onClick={() => setSlide(slide - 1)}>Prev</button>
+      :null}
+
+      {slide < props.slides.length-1 ? 
+      <button data-testid="button-next" onClick={() => setSlide(slide + 1)}>Next</button>
+      :null}
+
+      {slide !== 0 ? 
+      <button data-testid="button-restart" onClick={() => setSlide(0)}>Restart</button>
+      :null}
+
+    </div>
   )
 }
 
