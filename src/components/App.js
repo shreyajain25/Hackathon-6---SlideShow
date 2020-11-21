aae4a36e65d70427e88285290676fa59ebf30cc4
Shreya <shreyaj2510@gmail.com>
2020-11-21 19:07:36 +0530

    add

--- src/components/App.js
+++ src/components/App.js
@@ -4,32 +4,30 @@ import Slide from './Slide';
 
 const App = (props) => {
   const [slide, setSlide] = useState(0);
-  // useEffect(()=>{
-  //   return
-  // })
+
   const decr = () =>{
-    return setSlide(slide - 1);
+    setSlide(slide - 1);
   }
   const incr = () =>{
-    return setSlide(slide + 1);
+    setSlide(slide + 1);
   }
   return (
     <div>
       {slide !== 0 ? 
-      <button data-testid="button-restart" onClick={() => setSlide(0)}>Restart</button>
+      <button className="Restart" data-testid="button-restart" onClick={() => setSlide(0)}>Restart</button>
       :null}
 
       {slide !== 0 ? 
-      <button data-testid="button-prev" onClick={decr}>Prev</button>
+      <button className="Prev" data-testid="button-prev" onClick={decr}>Prev</button>
       :null}
 
       {slide < props.slides.length-1 ? 
-      <button data-testid="button-next" onClick={incr}>Next</button>
+      <button className="Next" data-testid="button-next" onClick={incr}>Next</button>
       :null}
 
-      {/* <h1 data-testid="title">{props.slides[slide].title}</h1>
-      <p data-testid="text">{props.slides[slide].text}</p> */}
-      <Slide title={props.slides[slide].title} text={props.slides[slide].text}></Slide>
+      <h1 data-testid="title">{props.slides[slide].title}</h1>
+      <p data-testid="text">{props.slides[slide].text}</p>
+      {/* <Slide title={props.slides[slide].title} text={props.slides[slide].text}></Slide> */}
 
     </div>
   )
