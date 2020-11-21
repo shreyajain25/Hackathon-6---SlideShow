253e3dae3a8e7a2499153673f6bad40b2bfebc36
Shreya <shreyaj2510@gmail.com>
2020-11-21 18:03:56 +0530

    function created

--- src/components/App.js
+++ src/components/App.js
@@ -7,23 +7,29 @@ const App = (props) => {
   // useEffect(()=>{
   //   return
   // })
+  const decr = () =>{
+    return setSlide(slide - 1);
+  }
+  const incr = () =>{
+    return setSlide(slide + 1);
+  }
   return (
     <div>
-      {/* <h1 data-testid="title">{props.slides[slide].title}</h1>
-      <p data-testid="text">{props.slides[slide].text}</p> */}
-      <Slide title={props.slides[slide].title} text={props.slides[slide].text}></Slide>
-
-      {slide !== 0 ? 
-      <button data-testid="button-prev" onClick={() => setSlide(slide - 1)}>Prev</button>
-      :null}
-
-      {slide < props.slides.length-1 ? 
-      <button data-testid="button-next" onClick={() => setSlide(slide + 1)}>Next</button>
-      :null}
-
       {slide !== 0 ? 
       <button data-testid="button-restart" onClick={() => setSlide(0)}>Restart</button>
       :null}
+
+      {slide !== 0 ? 
+      <button data-testid="button-prev" onClick={decr}>Prev</button>
+      :null}
+
+      {slide < props.slides.length-1 ? 
+      <button data-testid="button-next" onClick={incr}>Next</button>
+      :null}
+
+      {/* <h1 data-testid="title">{props.slides[slide].title}</h1>
+      <p data-testid="text">{props.slides[slide].text}</p> */}
+      <Slide title={props.slides[slide].title} text={props.slides[slide].text}></Slide>
 
     </div>
   )
